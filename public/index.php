fba774e98d5f86eb0098e796144caabafd2e1373
陈鹏儒 <cpr.up@outlook.com>
2024-05-02 02:05:05 +0800

    更新： 1、使用面向对象的编程思想对路由跳转逻辑进行第三次重构，提高代码的复用性和可读性 2、增加通用的错误方法处理，让网站不仅能处理404错误 3、增加403错误视图页面，提高网站错误界面的用户友好性

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php 
     require "../helpers.php";
-    $uri = $_SERVER['REQUEST_URI'];                                  //捕获当前请求的URI
-    //inspectAndDie($uri);                                           //测试当前URI
     require basePath('router.php');
+    $router = new Router();
+    $routes = require basePath('routes.php');
+    $uri = $_SERVER['REQUEST_URI'];                                  //捕获当前请求的URI
+    $method = $_SERVER['REQUEST_METHOD'];                            //捕获当前请求的方法
+    //inspect($uri);                                                 //测试当前URI（仅开发环境使用）
+    //inspect($method);                                              //测试当前方法（仅开发环境使用）
+    $router->route($uri, $method);
 ?>
--- router.php
+++ router.php
@@ -1,10 +1,38 @@
 <?php
-    $routes = require basePath('routes.php');                        //加载路由跳转规则
-    if(array_key_exists($uri, $routes)){                             //如果请求的路径有效，则返回相应的视图文件，否则报404错误
-         require(basePath($routes[$uri]));
-    }
-    else{
-        http_response_code(404);                                     //遇到404错误时返回HTTP状态码404
-        require (basePath($routes['404']));
+    class Router{                                                    //封装路由处理逻辑
+        protected $routes = [];                                      //定义路由数组
+        private function registerRoute($method, $uri, $controller){  //将路由信息添加到路由数组中
+            $this->routes[] = [
+                "method"=> $method,
+                "uri" => $uri,
+                "controller" => $controller
+            ];
+        }
+        public function addGet($uri, $controller){                   //添加一个GET路由
+            $this->registerRoute('GET', $uri, $controller);
+        }
+        public function addPost($uri, $controller){                  //添加一个POST路由
+            $this->registerRoute('POST', $uri, $controller);
+        }
+        public function addPut($uri, $controller){                   //添加一个PUT路由
+            $this->registerRoute('PUT', $uri, $controller);
+        }
+        public function addDelete($uri, $controller){                //添加一个DELETE路由
+            $this->registerRoute('DELETE', $uri, $controller);
+        }
+        public function route($uri, $method){                        //遍历路由数组，并匹配对应的路由
+            foreach($this->routes as $route){
+                if($route['uri'] === $uri && $route['method'] === $method){     //如果该次路由信息全部匹配成功
+                    require basePath($route['controller']);          //则返回对应的视图文件
+                    return;
+                }
+            }
+            $this->error();                                          //默认传递404错误代码
+        }
+        public function error($httpCode = 404){                      //默认处理404错误
+            http_response_code($httpCode);                           //遇到404错误时返回HTTP状态码404
+            loadView("error/{$httpCode}");
+            exit;                                                    //终止脚本的执行
+        }
     }
 ?>
